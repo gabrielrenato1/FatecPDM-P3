2e8510d71019fa2eca2e3377315afaaf458d940e
Gabriel Carvalho <gabrielrenatosc@gmail.com>
2025-07-04 20:17:45 -0300

    feat: faz busca em api interna

--- front/previsao_tempo/src/App.jsx
+++ front/previsao_tempo/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import Busca from './components/Busca'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='grid justify-content-center'>
+      <div className="col-12">
+        <Busca/>
+      </div>
+    </div>
   )
 }
 
---
+++ front/previsao_tempo/src/components/Busca.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react"
+import { InputText } from 'primereact/inputtext'
+import { RadioButton } from 'primereact/radiobutton';
+import openweatherClient from '../utils/openweatherClient'
+
+function Busca() {
+
+    const [termoBusca, setTermoBusca] = useState("-23.561167625063238, -46.65648357473211")
+    const [tipoBusca, setTipoBusca] = useState(null)
+    const [resultados, setResultados] = useState([])
+
+    useEffect(() => {
+
+        const fazerBusca = async () => {
+
+            const { data } = await openweatherClient.get('/search', {
+                params: {
+                    query: termoBusca,
+                    data_type: tipoBusca
+                }
+            })
+
+            setResultados(data)
+
+        }
+
+        if(termoBusca.length > 2 && termoBusca.includes(",") && tipoBusca !== null){
+
+            if(resultados.length === 0){
+                fazerBusca()
+            }else{
+
+                const timeoutID = setTimeout(() => {
+                    if(termoBusca){
+                        fazerBusca()
+                    }
+                }, 5000)
+
+                return () => {
+                    clearTimeout(timeoutID)
+                }
+                
+            }
+
+        }
+
+    }, [termoBusca, tipoBusca])
+
+    return (
+        <div className='flex flex-column m-2'>
+            <InputText
+                className="w-full border-1 border-round-sm text-lg"
+                placeholder="Digite a coordenada de uma cidade..."
+                onChange={(e) => {setTermoBusca(e.target.value)}}
+                value={termoBusca}
+            />
+
+            <div className="flex flex-wrap gap-3 w-full mt-3 justify-content-center">
+                <div className="flex align-items-center">
+                    <RadioButton inputId="buscaTipoTempMinMax" name="buscaRadio" value="temp_min_max" onChange={(e) => setTipoBusca(e.value)} checked={tipoBusca == "temp_min_max"} />
+                    <label htmlFor="buscaTipoTempMinMax" className="ml-2">Temperatura máxima e mínima</label>
+                </div>
+                <div className="flex align-items-center">
+                    <RadioButton inputId="buscaTipoHumidity" name="buscaRadio" value="humidity_atmosferic_pressure" onChange={(e) => setTipoBusca(e.value)} checked={tipoBusca == "humidity_atmosferic_pressure"} />
+                    <label htmlFor="buscaTipoHumidity" className="ml-2">Humidade e Pressão Atmosférica</label>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Busca
